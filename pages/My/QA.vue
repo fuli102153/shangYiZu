4346074542a2b0a4097e260e95773a7a8140e1b9
付力 <1102045342@qq.com>
2020-11-22 11:18:54 +0800

    头条

--- pages/My/QA.vue
+++ pages/My/QA.vue
@@ -1,15 +1,15 @@
 <template>
 	<view class="v-qa">
-		<view class="qa-card" v-for="(item, index) in 8" :key="index">
+		<view class="qa-card" v-for="(item, index) in list" :key="index">
 			<view class="header">
 				
 			</view>
 			<view class="content">
 				<view class="content-q">
-					Q：问题内容问题内容？
+					{{ item.q }}
 				</view>
 				<view class="content-a">
-					A：解答内容解答内容
+					{{ item.a }}
 				</view>
 			</view>
 		</view>
@@ -17,6 +17,19 @@
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				list: []
+			}
+		},
+		onLoad() {
+			this.list = this.Configs.question_answer.split("\n\n").map(item => {
+				return { q: item.split("\n")[0], a: item.split("\n")[1] }
+			})
+			console.log(this.list)
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
@@ -39,24 +52,22 @@
 				padding-top: 16rpx;
 				margin-left: 23rpx;
 				.content-q {
-					height: 60rpx;
 					background: #F5F8FA;
 					border: 1rpx solid #F0F2F4;
 					border-radius: 0px 30rpx 30rpx 30rpx;
 					color: #1676FE;
 					display: flex;
 					align-items: center;
-					padding: 0 45rpx;
+					padding: 5rpx 45rpx;
 				}
 				.content-a {
-					height: 60rpx;
 					background: #F5F8FA;
 					border: 1rpx solid #F0F2F4;
 					border-radius: 0px 30rpx 30rpx 30rpx;
 					color: #888888;
 					display: flex;
 					align-items: center;
-					padding: 0 45rpx;
+					padding: 5rpx 45rpx;
 					margin-top: 24rpx;
 				}
 			}
